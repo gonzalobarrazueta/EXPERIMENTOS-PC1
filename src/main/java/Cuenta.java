61151d41e0c48b262a4e1cdecf4d3877311e3254
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-04-18 21:36:36 -0500

    Create project

---
+++ src/main/java/Cuenta.java
@@ -0,0 +1,38 @@
+public class Cuenta {
+    private int saldo;
+    private String contra;
+
+    public Cuenta(int saldoini, String cc) { //constructor
+        saldo = saldoini;
+        contra = cc;
+    }
+
+    public int deposita(int q) {//receptor de dinero
+        saldo += q;
+        return saldo;
+    }
+
+    // q: cantidad que quiero retirar
+    // cc: contraseña
+    // retira si la contraseña coincide y hay fondos if (contra.equals(cc))
+    public int retira(int q, String cc) {
+
+        if (contra.equals(cc)) {
+            if (saldo >= q) {
+                return q;
+            } else return -1;
+        } else {
+            return -1;
+        }
+    }
+
+    // verifica saldo
+    public int dimeSaldo() {
+        return saldo;
+    }
+
+    // verifica contraseña
+    public String dimeContra() {
+        return contra;
+    }
+}
